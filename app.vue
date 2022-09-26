7e9b5dbefd4259df6dd0f3069e9c2cf6debece11
Hans Hofner <hhofner@gmail.com>
2022-09-26 21:24:32 +0900

    Better mobile support

--- app.vue
+++ app.vue
@@ -1,17 +1,19 @@
 <template>
   <h1>Quick Input</h1>
   <form>
-    <input type="date" v-model="inputDate" />
-    <input type="time" v-model="inputTime" />
+    <div class="dates">
+      <input type="date" v-model="inputDate"/>
+      <input type="time" v-model="inputTime"/>
+    </div>
     <div class="priceDetails">
-      <label class="price">
-        ¥ <input type="number" placeholder="Price" value="inputPrice" />
-      </label>
+      <input class="price" type="text" inputmode="numeric" placeholder="Price in yen" pattern="[0-9]*"
+             v-model="inputPrice"/>
       <select>
         <option value="">Payment Type</option>
         <option value="cash">Cash</option>
         <option value="paypay">PayPay</option>
-        <option value="mizuho-cc">Mizuho CC</option>
+        <option value="line-cc">Line Visa</option>
+        <option value="mizuho-cc">Mizuho Amex</option>
         <option value="mizuho-db">Mizuho DC</option>
       </select>
       <select>
@@ -22,13 +24,18 @@
         <option value="presents">Presents</option>
       </select>
     </div>
-    <textarea placeholder="remarks" />
+    <textarea placeholder="remarks"/>
     <button>Submit</button>
   </form>
 </template>
 
 <script>
 export default {
+  head:
+      {
+        meta: {name: "viewport", content: "width=device-width, initial-scale=1, maximum-scale=1"}
+      }
+  ,
   data() {
     return {
       inputDate: "",
@@ -49,17 +56,40 @@ export default {
   --medium-spacing: 0.6rem;
   --large-spacing: 1rem;
   --very-large-spacing: 2rem;
+
+}
+
+html {
+  max-width: 70ch;
+  padding: 3em 1em;
+  margin: auto;
+  line-height: 1.75;
+  font-size: 1.25em;
 }
 
 body {
-  max-width: 400px;
   margin: 0 auto;
 }
 
 form {
-  padding: 0 var(--small-spacing);
   gap: var(--large-spacing);
   display: flex;
   flex-direction: column;
 }
+
+.price {
+}
+
+.dates {
+  width: 100%;
+  display: flex;
+  gap: var(--small-spacing);
+}
+
+@media (max-width: 500px) {
+  input, textarea, button {
+    font-size: 1.2em;
+  }
+}
+
 </style>
